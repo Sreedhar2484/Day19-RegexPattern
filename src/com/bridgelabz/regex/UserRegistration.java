4ae931f30dd107e7664c9ceb69f73f9d3546e757
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-02-13 11:33:16 +0530

    program for regex patter for Last Name

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -13,15 +13,15 @@ public class UserRegistration {
         System.out.println("welcome to User Registration program");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter your first name:");
+        System.out.println("Enter your Last name:");
         String input = scanner.next();
         String regex = "^[A-Z]{1}+[a-z]{2,}+$";
         userName(regex, input);
 
         if (input.matches(regex) == true)
-            System.out.println("valid first name");
+            System.out.println("Valid Last name");
         else
-            System.out.println("invalid first name");
+            System.out.println("Invalid Last name");
 
     }
 }
